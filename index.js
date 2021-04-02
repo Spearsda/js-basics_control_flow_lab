3692b2b370ce80dca7f27c827092ebabfff3c42b
Damian <spearsda16@gmail.com>
2021-04-01 23:12:29 -0400

    Done with assingment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return "This one is on me!"
+  }
+  if (someValue > 2000 && someValue<2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+
+  else {
+    return 'No can do.'
+  }
+
+ 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+
+  const message = city === 'NYC' ? ("Ok, sounds good.") : ("No go.");
+
+  return message;
+
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+    
+    let message;
+    switch(tip){
+    
+    case 'generous':
+    message = "Thank you so much.";
+    break;
+
+    case 'not as generous':
+    message = "Thank you.";
+    break;
+
+    default:
+    message = "Bye.";
+    break;
+    }
+        return message;
+  
 }
